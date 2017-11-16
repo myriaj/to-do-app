b36543a5768924e42b634f073e8e3e80fe2a8f6f
Myria Clark <myriaj@yahoo.com>
2017-11-16 13:10:09 -0500

    Assignment 7 not working correctly

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function onReady() {
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
 
-    addToDoForm.addEventListener('submit', () event => {
+    addToDoForm.addEventListener('submit', event => {
       event.preventDefault();
 
       // get the text
@@ -24,12 +24,25 @@ function onReady() {
       // attach the checkbox to the li
       newLi.appendChild(checkbox);
 
+      // create a delete button
+      let deleteToDo = document.createElement('button');
+      deleteToDo.type = "button";
+      deleteToDo.textContent = "Delete To-Do!";
+
+      newLi.appendChild(deleteToDo);
+
       //attach the li to the ul
       toDoList.appendChild(newLi);
 
       //empty the input
       newToDoText.value = '';
-      
+
+    });
+
+    toDoList.addEventListener('button', event => {
+      event.preventDefault();
+
+      toDoList.removeChild(newLi);
     });
 }
 
